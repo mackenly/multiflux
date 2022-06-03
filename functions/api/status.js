ba50522e535e87dba5e1ef4629ca1f52eb822b4c
Mackenly Jones <mackenly@tricitiesmediagroup.com>
2022-06-03 15:32:45 -0400

    Fixed wrangler issues + added wrangler to startup

--- functions/api/status.js
+++ functions/api/status.js
@@ -1,6 +1,13 @@
-// Cloudflare Function to Return Status
-
-export async function onRequestPost(request) {
-    // ...
-    return new Response(`Okay`);
+export async function onRequest(context) {
+    // Contents of context object
+    const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+    } = context;
+  
+    return new Response("Hello, world!");
   }
