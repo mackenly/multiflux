b8c5d682f86a482bd907c372bd6248de04a3ce7e
Mackenly Jones <mackenly@tricitiesmediagroup.com>
2022-06-07 16:19:45 -0400

    Adds color bars

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -7,44 +7,64 @@
 // the Business Source License, use of this software will be governed
 // by the GNU AFFERO GENERAL PUBLIC LICENSE 3.0.
 
-import { useState, useEffect } from 'react';
-import { Stream } from '@cloudflare/stream-react';
+import { useState, useEffect } from "react";
+import { Stream } from "@cloudflare/stream-react";
 
-import cfstream from '../../assets/cfstream.svg';
+import cfstream from "../../assets/cfstream.svg";
+import colorbars from "../../assets/colorbars.png";
 
 export function Preview() {
-    const [streamName, setStreamName] = useState("");
-    const [streamId, setStreamId] = useState("");
+  const [streamName, setStreamName] = useState("");
+  const [streamId, setStreamId] = useState("");
+  const [streamingState, setStreamingState] = useState("disconnected");
 
-    useEffect(() => {
-        // update every second
-        const interval = setInterval(() => {
-            try {
-                setStreamName("- " + JSON.parse(sessionStorage.getItem("streamData")).result.meta.name);
-                setStreamId(JSON.parse(sessionStorage.getItem("streamOutputs")).result[0].uid);
-            } catch (e) { }
-        }
-        , 1000);
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    // update every second
+    const interval = setInterval(() => {
+      try {
+        const data = JSON.parse(sessionStorage.getItem("streamData"));
+        const outs = JSON.parse(sessionStorage.getItem("streamOutputs"));
+        setStreamName("- " + data.result.meta.name);
+        setStreamId(outs.result[0].uid);
+        setStreamingState(data.result.status.current.state);
+      } catch (e) {}
+      switch (streamingState) {
+        case "connected":
+          // streaming:
+          break;
+        case "disconnected":
+          // not streaming:
+          break;
+        default:
+          // unknown:
+          break;
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-
-    return (
-        <div className="Preview">
-            <div className="Preview-header">
-                <div className="Preview-header-status">
-                    <svg height="20" width="20">
-                        <circle cx="10" cy="10" r="10" fill="red" />
-                    </svg>
-                    <h2>Live Preview {streamName}</h2>
-                </div>
-                <div className="Preview-header-icons">
-                    <img src={cfstream} alt="cfstream" />
-                </div>
-            </div>
-            <div className="Preview-content">
-                {streamId !== "" ? <Stream controls autoplay src={streamId} /> : <div>Not Streaming...</div>}
-            </div>
+  return (
+    <div className="Preview">
+      <div className="Preview-header">
+        <div className="Preview-header-status">
+          <svg height="20" width="20">
+            <circle cx="10" cy="10" r="10" fill="red" />
+          </svg>
+          <h2>Live Preview {streamName}</h2>
         </div>
-    )
+        <div className="Preview-header-icons">
+          <img src={cfstream} alt="cfstream" />
+        </div>
+      </div>
+      <div className="Preview-content">
+        {streamingState === "connected" ? (
+          <Stream controls autoplay src={streamId} />
+        ) : (
+          <div className="notStreaming">
+            <p>{ streamingState }...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
